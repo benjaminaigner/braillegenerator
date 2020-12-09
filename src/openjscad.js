ca87a394229fd2ae4f07ea36c08d38417a732bc1
Benjamin Klaus <klaus@technikum-wien.at>
2020-12-09 14:08:09 +0100

    fixed baseurl

--- src/openjscad.js
+++ src/openjscad.js
@@ -477,7 +477,7 @@ OpenJsCad.parseJsCadScriptASync = function(script, mainParameters, options, call
     "src/openjscad.js"
   ];
 
-  var baseurl = document.location.href.replace(/\?.*$/, '') + brailleGenerator.BASE_PATH_PREFIX;
+  var baseurl = location.href.substring(0, location.href.lastIndexOf("/") + 1) + brailleGenerator.BASE_PATH_PREFIX;
   var openjscadurl = baseurl;
   if (typeof options['openJsCadPath'] != 'undefined') {
     // trailing '/' indicates it is a folder. This is necessary because makeAbsoluteUrl is called
